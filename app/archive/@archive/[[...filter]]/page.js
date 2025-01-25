d9b73f457c7cdb3a52bf92a69470ca5a46a74f95
clshu <clshu2020@gmail.com>
2025-01-25 06:14:19 -0800

    Catch-All Fallback Routes & Dealing With Multiple Path Segments

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,36 +1,58 @@
-import { getAvailableNewsYears } from '@/lib/news'
+import {
+  getAvailableNewsYears,
+  getNewsForYear,
+  getAvailableNewsMonths,
+  getNewsForYearAndMonth,
+} from '@/lib/news'
 import Link from 'next/link'
-// import NewsList from '@/components/news-list'
-// import { getNewsForYear } from '@/lib/news'
+import NewsList from '@/components/news-list'
 
-export default function FilteredNewsPage({ params }) {
-  const { filter } = params
-  console.log('filter', filter)
+export default async function FilteredNewsPage({ params }) {
+  const { filter } = await params
+  const selectedYear = filter?.[0]
+  const selectedMonth = filter?.[1]
 
-  const years = getAvailableNewsYears()
+  let news
+  let links = getAvailableNewsYears()
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear)
+    links = getAvailableNewsMonths(selectedYear)
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth)
+    links = []
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />
+  }
 
   return (
     <>
-      <header id="archive-header">
-        <h2> Archive Years</h2>
-        <nav>
-          <ul>
-            {years.map((year) => (
-              <li key={year}>
-                <Link href={`/archive/${year}`}>{year}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </header>
+      <>
+        <header id="archive-header">
+          <nav>
+            <ul>
+              {links.map((link) => {
+                const href = selectedYear
+                  ? `/archive/${selectedYear}/${link}`
+                  : `/archive/${link}`
+
+                return (
+                  <li key={link}>
+                    <Link href={href}>{link}</Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </nav>
+        </header>
+      </>
+      {newsContent}
     </>
   )
-  // const news = getNewsForYear(filter)
-
-  // return (
-  //   <>
-  //     <h2>Year {year} </h2>
-  //     <NewsList news={news} />
-  //   </>
-  // )
 }
