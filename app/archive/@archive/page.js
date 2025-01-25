0c78a9d3aa061da71b1e2068700f5233664d7d81
clshu <clshu2020@gmail.com>
2025-01-24 16:10:20 -0800

    Working with Parallel Routes & Nested Routes

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,7 +1,23 @@
+import Link from 'next/link'
+import { getAvailableNewsYears } from '@/lib/news'
+
 export default function ArchivePage() {
+  const years = getAvailableNewsYears()
+
   return (
-    <div>
-      <h1>Archive Page</h1>
-    </div>
+    <>
+      <header id="archive-header">
+        <h2> Archive Years</h2>
+        <nav>
+          <ul>
+            {years.map((year) => (
+              <li key={year}>
+                <Link href={`/archive/${year}`}>{year}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </>
   )
 }
---
+++ components/news-list.js
@@ -0,0 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+
+export default function NewsList({ news }) {
+  return (
+    <>
+      <ul className="news-list">
+        {news.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
